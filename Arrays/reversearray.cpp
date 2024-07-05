b80c42a61efd067cf9d034b806715ddcb1b06b3c
Rupinder Murmu <rupindermurmu@gmail.com>
2024-07-05 22:10:06 +0530

    Update reversearray.cpp
    Reverse the Arrays element

--- Arrays/reversearray.cpp
+++ Arrays/reversearray.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Reverse function
 void reverse(int arr[],int n){
     int start=0;
     int end=n-1;
@@ -13,6 +14,8 @@ void reverse(int arr[],int n){
 
   
 }
+
+// Printing reverse Array
 void printArray(int arr[],int n){
 reverse(arr,n);
     for(int i=0;i<n;i++){
