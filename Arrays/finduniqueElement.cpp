d8a7375de1ad48a84d58c3aff7d88f2ca8fc25ba
Rupinder Murmu <rupindermurmu@gmail.com>
2024-09-20 20:18:56 +0530

    Add files via upload

---
+++ Arrays/finduniqueElement.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findUnique(int arr[],int size){
+    int ans=0;
+    for(int i=0;i<size;i++){
+        ans=ans^arr[i];
+    }
+    return ans;
+    
+}
+
+int main(){
+    int n;
+    cin>>n; 
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+    cout<<"Unique element: "<<findUnique(arr,n);
+    
+    return 0;
+}
